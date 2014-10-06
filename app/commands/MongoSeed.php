81f4f64dc4a3050aa04f4a5c24d3765852a143f4
Michel Carroll <carrmi.0@gmail.com>
2014-10-05 23:14:20 -0400

    new airports

--- app/commands/MongoSeed.php
+++ app/commands/MongoSeed.php
@@ -27,6 +27,13 @@ class MongoSeed extends Command {
 	{
             $this->info("Starting to seed the mongo database");
             
+            $this->createAirport('ATL', 'Atlanta', 'Hartsfield-Jackson Atlanta International');
+            $this->createAirport('PEK', 'Beijing', 'Beijing Capital International Airport ');
+            $this->createAirport('LHR', 'London', 'London Heathrow');
+            $this->createAirport('BKK', 'Bangkok', 'Bangkok International Suvarnabhumi Airport');
+            $this->createAirport('DMK', 'Bangkok', 'Bangkok Don Mueang International Airport');
+            $this->createAirport('TYO', 'Tokyo', 'Tokyo Narita');
+            $this->createAirport('HND', 'Tokyo', 'Tokyo Haneda');
             $this->createAirport('YUL', 'Montreal', 'Pierre Elliot Trudeau Airport');
             $this->createAirport('YYZ', 'Toronto', 'Toronto Pearson International Airport');
             
